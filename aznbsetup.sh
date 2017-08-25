f1041d4dfb8b404ba06bae427d2e2b62f4ad14f8
Matt Hall <matt@agilegeoscience.com>
2017-08-24 23:34:29 -0300

    back to pip

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-#sudo pip --proxy http://webproxy:3128 install bruges
-#sudo pip --proxy http://webproxy:3128 install obspy
-#sudo pip --proxy http://webproxy:3128 install geopandas
-#sudo pip --proxy http://webproxy:3128 install folium
-
-# Pff, let's go back to conda
-
-echo 'proxy servers:
-    http: http://webproxy:3128
-    https: https://webproxy:3128
-' > $HOME/.condarc
-
 # Need to activate environment first
 source /home/nbcommon/anaconda3_410/bin/activate
 
-# Now installs... 
-conda config --add channels conda-forge
-conda install obspy
+# pip
+pip --proxy http://webproxy:3128 install bruges
+pip --proxy http://webproxy:3128 install obspy
+pip --proxy http://webproxy:3128 install geopandas
+pip --proxy http://webproxy:3128 install folium
 
-conda install geopandas
-conda install folium
+# conda
+# Can't get this to work :(
 
-pip install bruges
+#echo 'proxy servers:
+#    http: http://webproxy:3128
+#    https: https://webproxy:3128
+#' > $HOME/.condarc
+
+#conda config --add channels conda-forge
+#conda install obspy
+#conda install geopandas
+#conda install folium
 
